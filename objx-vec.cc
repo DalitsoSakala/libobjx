e65601df2f6d2e118a20879f8c9f80cfd2bd2ee5
dalitsosakala <dalitso.1sc@gmail.com>
2021-06-26 10:45:07 +0200

    converted Vec::x and Vec::y to pointers

--- objx-vec.cc
+++ objx-vec.cc
@@ -3,26 +3,26 @@
 #include<cassert>
 using namespace phys;
  Vec Vec:: operator+(float a)const{
-  return Vec(this->x+a,this->y+a);
+  return Vec(*x+a,*y+a);
  }
  Vec Vec:: operator+(Vec a){
-   return Vec(this->x+a.x,this->y+a.y);
+   return Vec(*x+*(a.x),*y+*(a.y));
  }
  float Vec::operator*(Vec a){
-  float p=a.x*this->x;
-  float p1=a.y*this->y;
+  float p=*(a.x)*(*x);
+  float p1=*(a.y)*(*y);
   return p+p1;
  }
  // Cross product
  Vec Vec::operator^(Vec a){
    return Vec();
  }
- float Vec::operator[](int a)const{
+ float& Vec::operator[](unsigned int a)const{
  assert(a<2&&a>=0);
-  return a==0?x:y;
+  return *(a==0?x:y);
  }
  Vec Vec:: operator/(float a)const{
-   return Vec(this->x/a,this->y/a);
+   return Vec(*x/a,*y/a);
  }
  // Get the angle floator the dot product
  float Vec:: operator%(Vec a){
@@ -30,10 +30,20 @@ using namespace phys;
   return acos(g/(this->abs()*a.abs())); 
  }
  float Vec::abs() const{
-   return sqrt(pow(this->x,2)+pow(this->y,2));
+   return sqrt(pow(*x,2)+pow(*y,2));
  }
 Vec Vec::unit() const {
   return *(this)/this->abs();
  }
-Vec:: Vec():x(0),y(0){}
-Vec:: Vec(float x,float y):x(x),y(y){}
+Vec:: Vec():x(new float),y(new float){
+ *(this->x)=0;
+ *(this->y)=0;
+}
+Vec:: Vec(float x,float y):Vec(){
+ *(this->x)=x;
+ *(this->y)=y;
+}
+Vec::~Vec(){
+ delete x;
+ delete y;
+}
--- objx-vec.h
+++ objx-vec.h
@@ -2,18 +2,20 @@
 
 namespace phys{
 struct Vec{
+ float *x,*y;
  Vec  operator+(float a)const;
  Vec  operator+(Vec a);
  float operator*(Vec a);
  // Cross product
  Vec operator^(Vec a);
- float operator[](int a)const;
+ float& operator[](unsigned int a)const;
  Vec  operator/(float a)const;
  // Get the angle floator the dot product
  float  operator%(Vec a);
  float abs() const;
-Vec unit();
+Vec unit()const;
  Vec();
  Vec(float x,float y);
+ ~Vec();
 };
 }
